08a1a6a3371b31dd2e6f4dca5f1ee0429577bbbb
vbouysso <102827290+vbouysso@users.noreply.github.com>
2022-04-03 15:49:47 -0700

    Session 1 Prompt Coding Journal 1

---
+++ printout.py
@@ -0,0 +1,5 @@
+print ("Name: Vincent Boussounouse")
+print("Pronouns: They/Them and She/Her")
+print("I don't have a favorite movie but one show I enjoy is 'The Disastrous Life of Saiki K.'")
+print("My favorite food is neopolitan ice cream.")
+
