77ac72c1692d746408030ddaffeb8bf1dd0844ba
vbouysso <102827290+vbouysso@users.noreply.github.com>
2022-04-21 18:42:53 -0700

    Session 5 Prompt Code Journal 1

---
+++ session_five.py
@@ -0,0 +1,19 @@
+import numpy as np
+import math
+
+def tabulatesinx():
+    valuearray = np.linspace(0, (2*math.pi), num=1000)
+    print("x                     sin(x)")
+    for i in valuearray:
+        formati = (format(i,".14f"))
+        formatsini = (format(math.sin(i),".14f"))
+        
+        print(formati, "    ", formatsini)
+
+    
+def main():
+    
+    tabulatesinx();
+
+if __name__=="__main__":
+    main()
